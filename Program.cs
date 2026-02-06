f848e165a31256a3e4a76c62e9bfe44b337ca8a7
yoga759 <nlputraa058@gmail.com>
2026-02-06 14:19:22 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Wizard
+{
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+    public Wizard(string name)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = 20;
+    }
+}
