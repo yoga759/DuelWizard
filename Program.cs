c19fcbdd0863a2195fc6a460fe43967e62c14314
yoga759 <nlputraa058@gmail.com>
2026-02-06 15:09:28 +0700

    Menambah Class Wizard, dua object Wizard, dan simulasi battle”

--- Program.cs
+++ Program.cs
@@ -1,13 +1,55 @@
-﻿class Wizard
-{
-    public string Name;
-    public int Energy;
-    public int Damage;
+﻿Wizard wizardA = new Wizard("Zilong", 50);
+Wizard wizardB = new Wizard("Argus",50);
 
-    public Wizard(string name)
+Console.WriteLine("Permainan di mulai...\n");
+Console.WriteLine("Statistik Awal");
+wizardA.showstats();
+wizardB.showstats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+wizardA.Attack(wizardB);
+
+Console.WriteLine("Permainan selesai...");
+Console.WriteLine("Statistik akhir");
+wizardA.showstats();
+wizardB.showstats();
+
+if (wizardA.energy > wizardB.energy)
+{
+    Console.WriteLine("Zilong pemenangnya");
+}
+else
+{
+ Console.WriteLine("Victory");
+    return;
+}
+public class Wizard
+{
+    //field
+    public string Name;
+    public int energy;
+    public int damage;
+
+    //constructor
+    public Wizard(string name,int damage)
     {
         Name = name;
-        Energy = 100;
-        Damage = 20;
+        energy = 100;
+        this.damage = damage;
+    }
+
+    //method
+    public void showstats()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Energy: {energy}\n");
+    }
+
+    public void Attack(Wizard enemyobj)
+    {
+        enemyobj.energy -= damage;
+        Console.WriteLine($" {Name} menyerang {enemyobj}");
+        Console.WriteLine($"Sisa energi {enemyobj.Name}: {energy}");
     }
 }
